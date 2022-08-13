bb9df88d18014a60d7653729b3f5b29259a3afdc
shamsuddeen sadisu <shamsudeenb2@gmail.com>
2022-08-13 14:28:42 +0100

    make change

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const stateName = document.querySelector('input').value
     try{
-        const response = await fetch(`api/${stateName}`)
+        const response = await fetch(`https://nigeria-state-infor-api.herokuapp.com/api/${stateName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('#capital').innerText = data.Capital
+        document.querySelector('#slogan').innerText = data.Slogan
+        document.querySelector('#region').innerText = data.Slogan
+        document.querySelector('#zone').innerText = data.Slogan
+        document.querySelector('#area').innerText = data.Slogan
     }catch(error){
         console.log(error)
     }
